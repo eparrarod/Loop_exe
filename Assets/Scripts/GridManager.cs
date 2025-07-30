4e2e749561223b83cf757ed8be891dc1e785c530
Esteban Parra <estebanpr014@gmail.com>
2025-07-30 18:56:45 -0500

    Level Map Loading
    Level map can now be built from a char matrix describing what each tile should be.
    
    Added char to Tile Dictionary

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -1,17 +1,27 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class GridManager : MonoBehaviour{
 
     [SerializeField]
-    public GameObject[] tiles;
+    public GameObject[] tiles; //list of tiles
 
-    public GameObject[,] grid;
     [SerializeField]
-    public GameObject pivotPoint;
+    public char[] chars; // list of chars
+
+    public Dictionary<char, GameObject> tileMap; //Mapping from char to tile
+
+    public char[,] mapEncoded; // Level as a char matrix
+
+    [SerializeField]
+    public GameObject pivotPoint; // Backgorund of tile grid area
+
     public int rows;
     public int columns;
     public float xSpacing = 0.0f;
     public float ySpacing = 0.0f;
+
+    public GameObject[,] levelGrid;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start(){
@@ -25,14 +35,32 @@ public class GridManager : MonoBehaviour{
 
     // Create Grid
     void createGrid() {
+
+        tileMap = new Dictionary<char, GameObject>();
+        for (int i = 0; i < tiles.Length; i++) {
+            tileMap.Add(chars[i], tiles[i]);
+        }
+
+        //Hardcoded Level
+        mapEncoded = new char[,] { { 'G', '.', '.', '#', '.', '.', 'G' },
+    { '.', '.', '.', '#', '.', '.', '.' },
+    { '.', '.', '.', '#', '.', '.', '.' },
+    { '#', '#', '#', 'R', '#', '#', '#' },
+    { '.', '.', '.', '#', '.', '.', '.' },
+    { '.', '.', '.', '#', '.', '.', '.' },
+    { 'G', '.', '.', '#', '.', '.', 'G' } };
+
+        rows = mapEncoded.GetLength(0);
+        columns = mapEncoded.GetLength(1);
+
         // Get size of tile prefab
         Vector2 dimensions = tiles[0].GetComponent<SpriteRenderer>().size;
         float scale = tiles[0].GetComponent<Transform>().localScale.x;
         Debug.Log($"Prefab size: {dimensions}");
-        xSpacing = dimensions[0] * scale;
-        ySpacing = dimensions[1] * scale;
+        xSpacing = (dimensions[0] - 0.02f) * scale;
+        ySpacing = (dimensions[1] - 0.02f) * scale;
 
-        grid = new GameObject[rows, columns];
+        levelGrid = new GameObject[rows, columns];
 
         // Instantiate all the dice in the UI
         for (int y = 0; y < rows; y++) {
@@ -40,14 +68,49 @@ public class GridManager : MonoBehaviour{
                 Vector3 pivot = pivotPoint.transform.position;
 
                 float totalw = rows * xSpacing;
-                float totalh = rows * xSpacing;
+                float totalh = rows * ySpacing;
 
+                // compute space available
+
+                //Vector2 sizeBgk = pivotPoint.GetComponent<SpriteRenderer>().size;
+                //float scaleBkg = pivotPoint.GetComponent<Transform>().localScale.x;
+                //float widthBgk = sizeBgk.x * scaleBkg;
+                //float heightBgk = sizeBgk.y * scaleBkg;
+
+                //float horizontalSpace = widthBgk - totalw;
+                //float verticalSpace = heightBgk - totalh;
+
+                //float scaleHor = (totalw + horizontalSpace) / totalw;
+                //float scaleVer = (totalh + verticalSpace) / totalh;
+
+                //scale = Mathf.Min(scaleVer/2, scaleHor/2);
+
+                //xSpacing = xSpacing * scale;
+                //ySpacing = ySpacing * scale;
+
+
+                // center within the center of the background of the GridArea
                 pivot[0] = pivot[0] - (totalw / 2);
                 pivot[1] = pivot[1] - (totalh / 2);
 
                 Vector3 position = pivot + new Vector3(x * xSpacing, y * ySpacing, 0);
                 Debug.Log(position);
-                grid[x, y] = Instantiate(tiles[0], position, Quaternion.identity, transform);
+
+                GameObject currentTile;
+
+                switch (mapEncoded[x, y]) {
+                    case 'G':
+                        currentTile = Instantiate(tiles[1], position, Quaternion.identity, transform);
+                        break;
+                    case '#':
+                        currentTile = Instantiate(tiles[2], position, Quaternion.identity, transform);
+                        break;
+                    default:
+                        currentTile = Instantiate(tiles[0], position, Quaternion.identity, transform);
+                        break;
+                }
+
+                levelGrid[x, y] = currentTile;
             }
         }
     }
